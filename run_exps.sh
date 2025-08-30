b121552d2a777faf5424dd8fa335021c12a0d40c
Filip Sondej <26285707+filyp@users.noreply.github.com>
2025-08-30 14:12:23 +0200

    . bio main run

--- run_exps.sh
+++ run_exps.sh
@@ -3,11 +3,12 @@
 # Script to run multiple sbatch jobs with different experiment numbers
 # Usage: ./run_proj_num_grid_search2.sh
 
-for i in {0..27}; do
+for i in {0..17}; do
     echo "Submitting job for experiment number: $i"
     # sbatch ~/unlearning/example_job.sh "python src/main_runner.py --config-name=proj_num_grid_search2 --exp-num=$i"
     # sbatch ~/unlearning/example_job.sh "python src/main_runner.py --config-name=layer_search --exp-num=$i"
-    sbatch ~/unlearning/example_job.sh "python src/main_runner.py --config-name=main_comparison_llama --group-name=main_comparison_llama_29.08.25_2 --exp-num=$i"
+    # sbatch ~/unlearning/example_job.sh "python src/main_runner.py --config-name=main_comparison_llama --group-name=main_comparison_llama_29.08.25_2 --exp-num=$i"
+    sbatch ~/unlearning/example_job.sh "python src/main_runner.py --config-name=main_comparison_llama_bio --group-name=main_comparison_llama_bio --exp-num=$i"
     
     # Optional: Add a small delay between submissions to avoid overwhelming the scheduler
     sleep 1
