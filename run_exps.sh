6352e8b906988348356304127cf7947dd9da7887
Filip Sondej <26285707+filyp@users.noreply.github.com>
2025-08-31 11:57:25 +0200

    allow 3% disr

--- run_exps.sh
+++ run_exps.sh
@@ -3,7 +3,7 @@
 # Script to run multiple sbatch jobs with different experiment numbers
 # Usage: ./run_proj_num_grid_search2.sh
 
-for i in {0..16}; do
+for i in {20..29}; do
     echo "Submitting job for experiment number: $i"
     # sbatch ~/unlearning/example_job.sh "python src/main_runner.py --config-name=proj_num_grid_search2 --exp-num=$i"
     # sbatch ~/unlearning/example_job.sh "python src/main_runner.py --config-name=layer_search --exp-num=$i"
